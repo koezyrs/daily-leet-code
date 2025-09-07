d1e27bb2532a9d160e4b7cd205dd5115f35bf386
Duong <tuanduong152004@gmail.com>
2025-09-07 14:56:41 +0700

    sove Q3 Weekly Contest 466

--- problems/Weekly Contest 466/Q3/optimized_solution.cpp
+++ problems/Weekly Contest 466/Q3/optimized_solution.cpp
@@ -7,7 +7,50 @@ public:
     long long bowlSubarrays(vector<int> &nums)
     {
         int n = nums.size();
-        int res = 0;
+        long long res = 0;
+        int leftGreater[n];
+        int rightGreater[n];
+
+        stack<int> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            leftGreater[i] = -1;
+            rightGreater[i] = -1;
+        }
+
+        // Find left greater
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && nums[st.top()] <= nums[i])
+                st.pop();
+
+            if (!st.empty())
+                leftGreater[i] = st.top();
+
+            st.push(i);
+        }
+
+        while (!st.empty())
+            st.pop();
+
+        // Find right greater
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && nums[st.top()] <= nums[i])
+                st.pop();
+
+            if (!st.empty())
+                rightGreater[i] = st.top();
+
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (leftGreater[i] != -1 && rightGreater[i] != -1)
+                res++;
+        }
 
         return res;
     }
